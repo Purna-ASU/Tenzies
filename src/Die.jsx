02d0b36e87639d062a9d8c9784d1df2120c78ad8
Purna-ASU <ppeddire@asu.edu>
2023-08-22 14:39:40 -0700

    dice are initialized

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,13 +1,10 @@
-import React from "react"
-import "./App.css"
 
-export default function Die(props) {
-    const styles = {
-        backgroundColor: props.held ? "#59E391" : "white"
-    }
-    return (
-        <div className="die-face" onClick={props.hold} style={styles}>
+function Die(props) {
+    return(
+        <div className="die-face">
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
+
+export default Die
