cd8267f9c932cdf4553d75bd6e53019481c3ccc6
Purna-ASU <ppeddire@asu.edu>
2023-08-22 15:35:44 -0700

    addressed the warning by nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./Die";
 import "./App.css"
+import { nanoid } from "nanoid";
 
 function App() {
+  
+  const [dice,setDice] = useState(allNewDice())
+  
+  function allNewDice(){
+    const newDice = []
+    for(let i=0; i < 10; i++){
+      newDice.push({value: Math.ceil(Math.random() * 6),
+                    isHeld: false,
+                    id: nanoid()
+                  })
+    }
+    return newDice
+  }console.log(allNewDice())
+
+  function rollDice() {
+    setDice(allNewDice)
+  }
+  
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value}/>)
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
+        {diceElements}
       </div>
+      <button onClick={rollDice}>Roll</button>
     </main>
   )
 }
